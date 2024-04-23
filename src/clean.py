e68db2a5dcb10a9e2c8b90adfae9b6e0e0cc0728
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-04-23 09:26:01 +0100

    Created optimization modules. Optimized part-of-speech filtering.

--- src/clean.py
+++ src/clean.py
@@ -9,13 +9,21 @@ import pandas as pd
 
 from utils import read_json_mapping, txt_to_set
 
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s: %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
 wn_pos = {'J': wordnet.ADJ, 'V': wordnet.VERB,
           'N': wordnet.NOUN, 'R': wordnet.ADV}
 
 wn = WordNetLemmatizer()
 
 stopwords = set(stopwords.words('english')).union(
-    txt_to_set("../data/vocab/stopwords.txt"))
+    txt_to_set("./data/vocab/stopwords.txt"))
 
 
 def remove_stopwords(tokens):
@@ -32,8 +40,8 @@ def filter_pos(tokens, kept_pos):
 
 def map_vocab(tokens):
 
-    contractions = read_json_mapping("../data/vocab/contractions.json")
-    dropped_gs = read_json_mapping("../data/vocab/contractions.json")
+    contractions = read_json_mapping("./data/vocab/contractions.json")
+    dropped_gs = read_json_mapping("./data/vocab/contractions.json")
 
     # Initialize list of mapped
     # contractions
@@ -62,34 +70,42 @@ def remove_pos_tags(tokens):
     return [token for token, _ in tokens]
 
 
-def clean_lyrics(df: pd.DataFrame, **kwargs):
+def clean_lyrics(series: pd.DataFrame, **kwargs):
 
     # lowercase lyrics
-    df.lyrics = df.lyrics.str.lower()
+    logger.info("Lowercasing lyrics...")
+    series = series.str.lower()
 
+    series.dropna(how="any", inplace=True)
     # normalize encoding
-    df.lyrics.apply(lambda x: unicodedata.normalize('NFKD', x))
+    logger.info("Normalizing unicode...")
+    series.apply(lambda x: unicodedata.normalize('NFKD', x))
 
     # remove square brackets
     if kwargs.get("remove_square_brackets", True):
+        logger.info("Removing square brackets...")
         square_brackets_pattern = r"\[([^\[\]]*+(?:\[[^\[\]]*+])*+)\]"
-        df.lyrics = df.lyrics.str.replace(
+        series = series.str.replace(
             square_brackets_pattern, "", regex=True)
 
     # remove regular brackets
     if kwargs.get("remove_regular_brackets", True):
+        logger.info("Removing regular brackets...")
         regular_brackets_pattern = r"\([^)]*\)"
-        df.lyrics = df.lyrics.str.replace(
+        series = series.str.replace(
             regular_brackets_pattern, "", regex=True)
 
     # remove newline characters
-    df.lyrics = df.lyrics.str.replace("\n", " ")
+    logger.info("Removing newline characters...")
+    series = series.str.replace("\n", " ")
 
     # remove carriage return characters
-    df.lyrics = df.lyrics.str.replace("\r", " ")
+    logger.info("Removing carriage return characters...")
+    series = series.str.replace("\r", " ")
 
     # remove adlibs
     if kwargs.get("remove_adlibs", True):
+        logger.info("Removing adlibs...")
         adlibs = {'ah', 'aw', 'anh', 'ay', 'ayo', 'ayoh', 'aye',
                   'br', 'da', 'dae', 'do', 'er', 'goh', 'he',
                   'ho', 'lad', 'ladi', 'ladium', 'li', 'm',
@@ -97,51 +113,61 @@ def clean_lyrics(df: pd.DataFrame, **kwargs):
                   'woah', 'wo' 'h', 'wo', 'unh', 'uho',
                   'umah', 'yo', 'yuh'}
         adlibs_pattern = r'\b(?:' + '|'.join(adlibs) + r')\b'
-        df.lyrics = df.lyrics.str.replace(adlibs_pattern, "", regex=True)
+        series = series.str.replace(adlibs_pattern, "", regex=True)
 
         minimum_length_adlibs = {('\b[he]{3,}\b'), ('\b[hey]{4,}\b'), ('\b[i]{2,}\b'), ('\b[la]{3,}\b'),
                                  ('\b[na]{3,}\b'), ('\b[no]{5,}\b'), ('\b[ops]{4,}\b'), ('\b[bra]{4,}\b')}
         minimum_length_adlibs_pattern = fr"{'|'.join([sub_pattern for sub_pattern in minimum_length_adlibs])}"  # noqa
-        df.lyrics = df.lyrics.str.replace(
+        series = series.str.replace(
             minimum_length_adlibs_pattern, "", regex=True)
 
     # remove whitespace
+    logger.info("Removing whitespace...")
     whitespace_pattern = r"\s{2,}"
-    df.lyrics = df.lyrics.str.replace(whitespace_pattern, "", regex=True)
-    df.lyrics = df.lyrics.str.strip()
+    series = series.str.replace(whitespace_pattern, "", regex=True)
+    series = series.str.strip()
 
     # remove punctuation
+    logger.info("Removing punctuation...")
     punctuation_pattern = r"[^\w\s]"
-    df.lyrics = df.lyrics.str.replace(punctuation_pattern, "", regex=True)
+    series = series.str.replace(punctuation_pattern, "", regex=True)
 
     # word tokenize
-    df.lyrics = df.lyrics.apply(word_tokenize)
+    logger.info("Tokenizing lyrics...")
+    series = series.apply(word_tokenize)
 
     # map vocabulary
     if kwargs.get("map_vocab", True):
-        df.lyrics = df.lyrics.apply(map_vocab)
+        logger.info("Mapping vocabulary...")
+        series = series.apply(map_vocab)
 
     # tag pos
     if kwargs.get("filter_pos", True) or kwargs.get("pos_lemmatize", True):
-        df.lyrics = df.lyrics.apply(pos_tag)
+        logger.info("Tagging part-of-speech...")
+        series = series.apply(pos_tag)
 
     # pos filter
     if kwargs.get("filter_pos", True):
-        df.lyrics = df.lyrics.apply(
-            filter_pos, True, (kwargs.get("kept_pos", "N")))
+        logger.info("Filtering part-of-speech...")
+        series = series.apply(filter_pos, args=(kwargs.get("kept_pos", "N"),))
 
         if not kwargs.get("pos_lemmatize", True):
-            df.lyrics = df.lyrics.apply(remove_pos_tags)
+            series = series.apply(remove_pos_tags)
 
     # pos lemmatize
     if kwargs.get("pos_lemmatize", True):
-        df.lyrics = df.lyrics.apply(pos_lemmatize)
+        logger.info("Lemmatizing part-of-speech...")
+        series = series.apply(pos_lemmatize)
 
     # remove stopwords
     if kwargs.get("remove_stopwords", True):
-        df.lyrics = df.lyrics.apply(remove_stopwords)
+        logger.info("Removing stopwords...")
+        series = series.apply(remove_stopwords)
 
     # rejoin to string
-    df.lyrics = df.lyrics.str.join(" ")
+    joined_lyrics = series.str.join(" ").to_list()
 
-    return df
+    tokenized_lyrics = series.to_list()
+
+    logger.info("Finished cleaning lyrics!")
+    return joined_lyrics, tokenized_lyrics
