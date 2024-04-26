73c8f3cf98eec74cc5409781cf6d2e2c84e43d37
Charlie Marshall <charlie.marshall1996@gmail.com>
2024-04-26 09:25:20 +0100

    Extraction analysis

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(name="language of music",
       version="0.0.0a1",
       description="Topic modelling of lyrics.",
       author="Charlie Marshall",
-      author_email="charlie.marshall1996@gmail.com")
+      author_email="charlie.marshall1996@gmail.com",
+      packages=["src"])
